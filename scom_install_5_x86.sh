52f28fd2c6d35e286cf36462c8b0c8ab996e1c9f
Luther Barnum <luther_barnum@jabil.com>
2015-06-16 14:04:20 -0400

    changed rhel 5 script to handle correct version

--- scom_install_5_x86.sh
+++ scom_install_5_x86.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp files/scom.repo /etc/yum.repos.d/
-chmod 644 /etc/yum.repos.d/scom.repo
+cp files/scom_5_x86.repo /etc/yum.repos.d/
+chmod 644 /etc/yum.repos.d/scom_5_x86.repo
 yum install -y scx
 chkconfig scx-cimd on
 service scx-cimd restart
