456afb6291a4699ced6498b64be69c47ab044cc2
Luther Barnum <luther_barnum@jabil.com>
2015-06-17 12:28:05 -0400

    changed path from files/*

--- scom_install.sh
+++ scom_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp files/scom.repo /etc/yum.repos.d/
+cp scom.repo /etc/yum.repos.d/
 chmod 644 /etc/yum.repos.d/scom.repo
 yum install -y scx
 /sbin/chkconfig scx-cimd on
--- scom_install_5_x86.sh
+++ scom_install_5_x86.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum install -y epel-release
-cp files/scom_5_x86.repo /etc/yum.repos.d/
+cp scom_5_x86.repo /etc/yum.repos.d/
 chmod 644 /etc/yum.repos.d/scom_5_x86.repo
 yum install -y scx
 /sbin/chkconfig scx-cimd on
