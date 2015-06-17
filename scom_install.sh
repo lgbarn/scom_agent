378acc5249c7dd3b1fecc472772abfaba24fd2f5
Luther Barnum <luther_barnum@jabil.com>
2015-06-17 09:35:15 -0400

    added full path for commands

--- scom_install.sh
+++ scom_install.sh
@@ -3,5 +3,5 @@
 cp files/scom.repo /etc/yum.repos.d/
 chmod 644 /etc/yum.repos.d/scom.repo
 yum install -y scx
-chkconfig scx-cimd on
-service scx-cimd restart
+/sbin/chkconfig scx-cimd on
+/sbin/service scx-cimd restart
